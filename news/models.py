dbb8c276c114cd3c62a3162365949fd2d60a07a1
zakir <zakir95olymp@gmail.com>
2023-11-02 13:40:00 +0900

    fixed the createdDate variable in the models.py

--- news/models.py
+++ news/models.py
@@ -3,5 +3,9 @@ from django.db import models
 class PostsModel(models.Model):
     title = models.CharField(max_length=500, verbose_name='Please enter the post title')
     content = models.TextField(verbose_name='Please enter the post text')
-    createdDate = models.DateField()
+    createdDate = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
