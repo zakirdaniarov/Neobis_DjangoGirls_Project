929fa5efbf423b08996fc3435bb38229d4d552f1
zakir <zakir95olymp@gmail.com>
2023-11-06 23:34:51 +0900

    made some modifications in the urls file of the news app

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import postView, addPost, editPost, deletePost
+from .views import postView, addPost, editPost
 
 urlpatterns = [
     path('', postView),
     path('create_post/', addPost),
     path('edit_post/<int:i>/', editPost),
-    path('delete_post/<int:i>/', deletePost),
 ]
